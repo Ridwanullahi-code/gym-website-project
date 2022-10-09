92699f85f97aa893a32ee901664c732673835c42
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-10-09 22:18:19 +0100

    last update

---
+++ index.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.hamburger');
+var menuItems = document.querySelector('.menu-items');
+menuBtn.addEventListener('click',function showMenu(){
+    menuBtn.classList.toggle('active');
+    menuItems.classList.toggle('menu-show');  
+});
+
+// function to close menu items when any of one it clicked
+const navLinks = document.querySelectorAll('.nav-item');
+navLinks.forEach((link) => {
+    link.addEventListener('click', () =>{
+        menuItems.classList.remove('menu-show');
+    })
+});
+
+const main = document.querySelector('main');
+// backdrop function to close menu items
+    main.addEventListener('click',()=>{
+        menuItems.classList.remove('menu-show');
+    })
